28b712be17c04227ec3c093a00ef4cdaf9c5bcaf
Rutti <ruttib@gmail.com>
2022-05-30 11:47:56 -0400

    first commit

---
+++ src/main/java/io/javabrains/isthesiteup/controllers/RestServiceApplication.java
@@ -0,0 +1,14 @@
+package io.javabrains.isthesiteup.controllers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+}
+
